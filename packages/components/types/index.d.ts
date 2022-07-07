3a7059a0933aa1c951a50163f4682e08aad126e5
Nick <nickas951@gmail.com>
2022-07-07 12:28:23 +0200

    Remove types

--- packages/components/types/index.d.ts
+++
@@ -1,4 +0,0 @@
-export { default as Alert } from "./components/alert/Alert.svelte";
-export { default as AlertActions } from "./components/alert/AlertActions.svelte";
-export { default as AlertContent } from "./components/alert/AlertContent.svelte";
-export { default as AlertIcon } from "./components/alert/AlertIcon.svelte";
