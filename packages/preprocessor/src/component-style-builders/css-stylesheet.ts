1f344d6d7a4c0fb3e4d3fa5d535a58cf8356eeb1
Nick <nickas951@gmail.com>
2022-11-02 08:18:00 +0200

    Add media

--- packages/preprocessor/src/component-style-builders/css-stylesheet.ts
+++ packages/preprocessor/src/component-style-builders/css-stylesheet.ts
@@ -1,17 +1,23 @@
 export class CssStylesheet {
     public constructor(
-        public selector: string = '',
+        public mediaQuery?: string,
+        public selector: string = '*',
         private readonly rules: Map<string, string | number> = new Map(),
     ) {}
+
+    public media(mediaQuery: string): void {
+        this.mediaQuery = mediaQuery;
+    }
 
     public addRule(key: string, value: string | number): void {
         this.rules.set(key, value);
     }
 
-    public build(): string {
-        const rules = Array.from(this.rules.entries())
-            .map(([key, value]) => `${key}: ${value};`)
-            .join(' ');
-        return `${this.selector} { ${rules} }`;
+    public toString(): string {
+        const rules = [...this.rules.entries()].map(([key, value]) => `${key}: ${value};`).join(' ');
+
+        return `${this.mediaQuery ? `${this.mediaQuery} {` : ''}${this.selector} { ${rules} }${
+            this.mediaQuery ? '}' : ''
+        }`;
     }
 }
