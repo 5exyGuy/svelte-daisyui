a65d3939d1e933ae75d3ccdeea9f7872cd208364
Nick <nickas951@gmail.com>
2022-11-08 17:50:08 +0200

    Update

--- packages/preprocessor/src/component-style-builders/alert.style-builder.ts
+++ packages/preprocessor/src/component-style-builders/alert.style-builder.ts
@@ -1,17 +1,39 @@
 import { parseComponents } from './component-parser';
-import type { AlertProps } from '@svelte-daisyui/shared';
+import { AlertProps, AlertResponsiveProps, ScreenSize, ScreenSizeMinWidth } from '@svelte-daisyui/shared';
 import type { PreprocessorOptions } from '../interfaces/preprocess-options.interface';
+import type { StringKeyOf } from 'type-fest';
 
 const COMPONENT_NAME = 'Alert';
-const COLORS = ['primary', 'secondary', 'accent', 'info', 'success', 'warning', 'error', 'base'] as const;
+const COLORS = ['base', 'primary', 'secondary', 'accent', 'info', 'success', 'warning', 'error'] as const;
+const DEFAULT_COLOR = 'base';
 
 export function buildAlertStyles(code: string, options: PreprocessorOptions) {
-    const componentAttributes = parseComponents<AlertProps>(COMPONENT_NAME, code);
+    const components = parseComponents<AlertProps>(COMPONENT_NAME, code);
 
     const colors = new Set<Required<AlertProps>['color']>(options.includeDefaults ? COLORS : []);
+    options.screenSizes = options.screenSizes ? { ...ScreenSizeMinWidth, ...options.screenSizes } : ScreenSizeMinWidth;
 
-    componentAttributes.forEach((attributes) => {
-        if (options.includeDefaults && attributes.color) colors.add(attributes.color);
+    const screenSizes = {} as Record<
+        StringKeyOf<typeof ScreenSize>,
+        { [P in keyof AlertResponsiveProps]: Set<AlertResponsiveProps[P]> }
+    >;
+
+    components.forEach((component) => {
+        if (options.includeDefaults && component.color && !COLORS.includes(component.color))
+            colors.add(component.color);
+
+        if (!component.screen) return;
+        const parsedScreen = JSON.parse(component.screen) as AlertResponsiveProps;
+
+        (Object.keys(parsedScreen) as Array<StringKeyOf<typeof ScreenSize>>).forEach((screen) => {
+            if (!ScreenSize[screen]) return;
+
+            screenSizes[screen] = screenSizes[screen] ?? {};
+            if (component.screen[screen].color) {
+                screenSizes[screen].color = screenSizes[screen].color ?? new Set();
+                screenSizes[screen].color.add(component.screen[screen].color);
+            }
+        });
     });
 
     const colorStyles = options.includeDefaults
