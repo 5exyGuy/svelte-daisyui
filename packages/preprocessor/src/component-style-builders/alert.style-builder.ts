792e8cab89aa1e60b0fdaf10a5ed1123068d7a1e
Nick <nickas951@gmail.com>
2022-11-08 08:05:58 +0200

    Update Alert style builder

--- packages/preprocessor/src/component-style-builders/alert.style-builder.ts
+++ packages/preprocessor/src/component-style-builders/alert.style-builder.ts
@@ -3,16 +3,22 @@ import type { AlertProps } from '@svelte-daisyui/shared';
 import type { PreprocessorOptions } from '../interfaces/preprocess-options.interface';
 
 const COMPONENT_NAME = 'Alert';
+const COLORS = ['primary', 'secondary', 'accent', 'info', 'success', 'warning', 'error', 'base'] as const;
 
 export function buildAlertStyles(code: string, options: PreprocessorOptions) {
     const componentAttributes = parseComponents<AlertProps>(COMPONENT_NAME, code);
 
-    const colors = new Set<Required<AlertProps>['color']>();
-    const screenSizes = new Set<Required<AlertProps>['screenSize']>();
+    const colors = new Set<Required<AlertProps>['color']>(options.includeDefaults ? COLORS : []);
 
     componentAttributes.forEach((attributes) => {
-        if (attributes.color) colors.add(attributes.color);
+        if (options.includeDefaults && attributes.color) colors.add(attributes.color);
     });
 
-    return '';
+    const colorStyles = options.includeDefaults
+        ? '@include colors();\n'
+        : Array.from(colors)
+              .map((color) => `@include color(${color});`)
+              .join('\n');
+
+    return colorStyles;
 }
