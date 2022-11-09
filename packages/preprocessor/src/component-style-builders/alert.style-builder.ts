cffa82664255521835a54b06bd6b746a646ab6cc
Nick <nickas951@gmail.com>
2022-11-09 18:07:42 +0200

    Update Alert style builder

--- packages/preprocessor/src/component-style-builders/alert.style-builder.ts
+++ packages/preprocessor/src/component-style-builders/alert.style-builder.ts
@@ -2,17 +2,20 @@ import { parseComponents } from './component-parser';
 import type { AlertProps, AlertResponsiveProps, ScreenSizeNames } from '@svelte-daisyui/shared';
 import type { PreprocessorOptions } from '../interfaces/preprocess-options.interface';
 import { ALERT } from '../constants/alert.constants';
+import type { StringKeyOf } from 'type-fest';
 
 export function buildAlertStyles(code: string, options: Required<PreprocessorOptions>) {
     const components = parseComponents<AlertProps>(ALERT.COMPONENT_NAME, code);
 
     const colors = new Set<Required<AlertProps>['color']>(options.includeDefaults ? ALERT.COLORS : []);
-    const screenSizes = {} as Record<
-        ScreenSizeNames,
-        { [P in keyof AlertResponsiveProps]: Set<Exclude<AlertResponsiveProps[P], undefined>> }
-    >;
+    const screenSizes = {} as {
+        [SizeName in ScreenSizeNames]?: {
+            [Prop in StringKeyOf<AlertResponsiveProps>]?: Set<Exclude<AlertResponsiveProps[Prop], undefined>>;
+        };
+    };
 
     components.forEach((component) => {
+        // Include onl
         if (!options.includeDefaults) {
             const compColor = component.color as Exclude<AlertProps['color'], undefined>;
             const color = compColor && ALERT.COLORS.includes(compColor) ? compColor : ALERT.DEFAULT_COLOR;
@@ -20,17 +23,19 @@ export function buildAlertStyles(code: string, options: Required<PreprocessorOpt
         }
         if (!component.screen) return;
 
+        // Parsed component's screen attribute's value
         const parsedScreen = JSON.parse(component.screen) as Exclude<AlertProps['screen'], undefined>;
-
         Object.entries(parsedScreen).forEach((entry) => {
             const [screenSize, screenSizeProps] = entry as [ScreenSizeNames, AlertResponsiveProps];
             if (!options.screenSizes[screenSize]) return;
+            if (!screenSizeProps || typeof screenSizeProps !== 'object') return;
 
-            screenSizes[screenSize] = screenSizes[screenSize] ?? {};
-            const parsedScreenColor = screenSizeProps?.color;
-            if (parsedScreenColor && ALERT.COLORS.includes(parsedScreenColor)) {
-                screenSizes[screenSize].color = screenSizes[screenSize].color ?? new Set();
-                screenSizes[screenSize].color!.add(parsedScreenColor);
+            screenSizes[screenSize] = screenSizes[screenSize] || {};
+            const color = screenSizeProps?.color;
+
+            if (color && ALERT.COLORS.includes(color)) {
+                screenSizes![screenSize]!.color = screenSizes![screenSize]!.color || new Set();
+                screenSizes![screenSize]!.color!.add(color);
             }
         });
     });
@@ -40,6 +45,23 @@ export function buildAlertStyles(code: string, options: Required<PreprocessorOpt
         : Array.from(colors)
               .map((color) => `@include color(${color});`)
               .join('\n');
+    const screenStyles = Object.entries(screenSizes)
+        .map((entry) => {
+            const [screenSize, screenSizeProps] = entry as [
+                ScreenSizeNames,
+                { [Prop in StringKeyOf<AlertResponsiveProps>]?: Set<Exclude<AlertResponsiveProps[Prop], undefined>> },
+            ];
+            const color = screenSizeProps?.color;
+            return color
+                ? Array.from(color)
+                      .map(
+                          (color) =>
+                              `@include responsive-color(${color}, ${screenSize}, ${options.screenSizes[screenSize]});`,
+                      )
+                      .join('\n')
+                : '';
+        })
+        .join('\n');
 
-    return colorStyles;
+    return colorStyles + screenStyles;
 }
--- packages/preprocessor/src/interfaces/preprocess-options.interface.ts
+++ packages/preprocessor/src/interfaces/preprocess-options.interface.ts
@@ -3,4 +3,5 @@ import type { CSSUnit, ScreenSizeNames } from '@svelte-daisyui/shared';
 export interface PreprocessorOptions<T extends Record<keyof T, CSSUnit> = Record<string, CSSUnit>> {
     includeDefaults?: boolean;
     screenSizes?: Partial<Record<ScreenSizeNames, CSSUnit>> & Partial<T>;
+    includeAsGlobal?: boolean;
 }
