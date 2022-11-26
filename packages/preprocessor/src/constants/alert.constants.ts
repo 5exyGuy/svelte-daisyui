3b5dd86bbde2c86d18383cf342719d9cb1d25571
Nick <nickas951@gmail.com>
2022-11-26 19:24:07 +0200

    Update

--- packages/preprocessor/src/constants/alert.constants.ts
+++
@@ -1,5 +0,0 @@
-export const ALERT = {
-    COMPONENT_NAME: 'Alert',
-    COLORS: ['base', 'primary', 'secondary', 'accent', 'info', 'success', 'warning', 'error'],
-    DEFAULT_COLOR: 'base',
-} as const;
--- packages/shared/src/schemas/alert.schema.ts
+++ packages/shared/src/schemas/alert.schema.ts
@@ -1,4 +1,12 @@
 import { object, string } from 'joi';
 import type { AlertClassProps } from '../interfaces';
 
-export const AlertSchema = object<AlertClassProps>({ color: string().equal() });
+export const AlertSchema = {
+    name: 'Alert',
+    colors: ['base', 'primary', 'secondary', 'accent', 'info', 'success', 'warning', 'error'],
+    defaultColor: 'base',
+    propTypes: {
+        color: 'string',
+    } as Record<keyof AlertClassProps, string>,
+    schema: object<AlertClassProps>({ color: string().equal() }),
+} as const;
