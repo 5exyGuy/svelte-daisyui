beed0977e5c5c3a07b935df19b4f1a2c9a454f44
Nick <nickas951@gmail.com>
2023-01-21 18:12:50 +0200

    Add optimized deps to core package for better development process

--- packages/core/vite.config.js
+++ packages/core/vite.config.js
@@ -3,6 +3,13 @@ import { sveltekit } from '@sveltejs/kit/vite';
 /** @type {import('vite').UserConfig} */
 const config = {
     plugins: [sveltekit()],
+    optimizeDeps: {
+        include: [
+            '@svelte-daisyui/shared',
+            '@svelte-daisyui/preprocess',
+            // '@svelte-daisyui/styles'
+        ],
+    },
 };
 
 export default config;
