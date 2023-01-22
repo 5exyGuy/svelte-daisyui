ba3a3ec547d8dda53efa932fff446232927c5791
Nick <nickas951@gmail.com>
2023-01-22 19:40:43 +0200

    Update badge

--- packages/preprocess/src/builders/components/badge-style-builder.ts
+++ packages/preprocess/src/builders/components/badge-style-builder.ts
@@ -1,19 +1,37 @@
-import {
-    AvatarStatus,
-    BadgeSchema,
-    convertToEntries,
-    type BadgeProps,
-    type BadgeResponsivePropNames,
-} from '@svelte-daisyui/shared';
+import { BadgeSchema, convertToEntries, type BadgeProps, type BadgeResponsivePropNames } from '@svelte-daisyui/shared';
 import { createStyleBuilder } from '../create-style-builder';
 
 export const BadgeStyleBuilder = createStyleBuilder<BadgeProps, BadgeResponsivePropNames>(BadgeSchema, (context) => {
     context.useLib('badge');
 
     // Colors
-    convertToEntries(context.uniqueProps.color).forEach(([breakpointName, breakpointValues]) => {});
+    convertToEntries(context.uniqueProps.color).forEach(([breakpointName, breakpointValues]) => {
+        breakpointValues!.forEach((color) => {
+            breakpointName === 'default'
+                ? context.includeMixin('badge', 'color', color)
+                : context.includeMixin(
+                      'badge',
+                      'responsive-color',
+                      color,
+                      breakpointName,
+                      context.options.breakpoints[breakpointName] as string,
+                  );
+        });
+    });
     // Outline
     context.uniqueProps.outline?.forEach((outline) => outline && context.includeMixin('badge', 'outline'));
     // Size
-    convertToEntries(context.uniqueProps.size).forEach(([breakpointName, breakpointValues]) => {});
+    convertToEntries(context.uniqueProps.size).forEach(([breakpointName, breakpointValues]) => {
+        breakpointValues!.forEach((size) => {
+            breakpointName === 'default'
+                ? context.includeMixin('badge', 'size', size)
+                : context.includeMixin(
+                      'badge',
+                      'responsive-size',
+                      size,
+                      breakpointName,
+                      context.options.breakpoints[breakpointName] as string,
+                  );
+        });
+    });
 });
